31ee855d302751efe520ea80a28153c97737022e
Sven Erik Knop <seknop@gmail.com>
2019-07-05 12:11:28 +0200

    Works for generating, but cannot run with docker-compose
    Depends-on needs the correct indentation.

--- kafka_docker_composer.py
+++ kafka_docker_composer.py
@@ -15,20 +15,20 @@ DOCKER_COMPOSE_FILE="docker-compose.yaml"
 # known variables to fill
 # single (broker)
 #
-# {{broker-name}}
-# {{broker-id}}
-# {{broker-port-internal}}
-# {{broker-port-external}}
-# {{advertised-broker-port-external}}
-# {{advertised-broker-port-internal}}
-# {{broker-jmx-port}}
+BROKER_NAME="{{broker-name}}"
+BROKER_ID="{{broker-id}}"
+BROKER_PORT_INTERNAL="{{broker-port-internal}}"
+BROKER_PORT_EXTERNAL="{{broker-port-external}}"
+BROKER_ADVERTISED_PORT_EXTERNAL="{{broker-advertised-port-external}}"
+BROKER_ADVERTISED_PORT_INTERNAL="{{broker-advertised-port-internal}}"
+BROKER_JMX_PORT="{{broker-jmx-port}}"
 #
 # single (zookeeper)
 #
-# {{zookeeper-name}}
-# {{zookeeper-id}}
-# {{zookeeper-port}}
-# {{zookeeper-jxm-port}}
+ZOOKEEPER_NAME="{{zookeeper-name}}"
+ZOOKEEPER_ID="{{zookeeper-id}}"
+ZOOKEEPER_PORT="{{zookeeper-port}}"
+ZOOKEEPER_JMX_PORT="{{zookeeper-jmx-port}}"
 #
 # services
 #
@@ -37,10 +37,10 @@ BROKER_SERVICES="{{broker-services}}"
 #
 # multiple
 #
-# {{zookeeper-containers}} - array of dependencies
-# {{zookeeper-ports}}      - host:port[,host:port]*
-# {{zookeeper-internal-ports}} - host:2888:3888[;host:2888:3888]*
-#
+ZOOKEEPER_CONTAINERS="{{zookeeper-containers}}" # array of dependencies
+ZOOKEEPER_PORTS="{{zookeeper-ports}}" # host:port[,host:port]*
+ZOOKEEPER_INTERNAL_PORTS="{{zookeeper-internal-ports}}" # host:2888:3888[;host:2888:3888]*
+
 
 
 class YamlGenerator:
@@ -48,28 +48,105 @@ class YamlGenerator:
         self.args = args
 
         with open(self.args.docker_compose_template) as f:
-            self.master_template = f.read().split("\n")
+            self.master_template = f.read()
 
+        with open(self.args.broker_template) as f:
+            self.broker_template = f.read()
+
+        with open(self.args.zookeeper_template) as f:
+            self.zookeeper_template = f.read()
 
         self.zookeeper_offset = YamlGenerator.find_offset(self.master_template, ZOOKEEPER_SERVICES)
         self.broker_offset = YamlGenerator.find_offset(self.master_template, BROKER_SERVICES)
 
+        self.zookeeper_containers = ""
+        self.zookeeper_ports = ""
+        self.zookeeper_internal_ports = ""
+
     def generate(self):
+        zookeeper_services = self.generate_zookeeper_services()
+        broker_services = self.generate_broker_services()
+
+        zookeeper_placeholder = self.zookeeper_offset + ZOOKEEPER_SERVICES
+        broker_placeholder = self.broker_offset + BROKER_SERVICES
+
+        output_file = self.master_template
+        output_file = output_file.replace(zookeeper_placeholder, zookeeper_services)
+        output_file = output_file.replace(broker_placeholder, broker_services)
+
         with open(self.args.docker_compose_file, "w") as yaml_file:
-            for line in self.master_template:
-                yaml_file.write(line)
-                yaml_file.write('\n')
+            yaml_file.write(output_file)
 
     def generate_zookeeper_services(self):
-        pass
+        zookeepers = []
+
+        for zk in range(1,self.args.zookeepers + 1):
+            zookeeper = {}
+            zookeeper[ZOOKEEPER_NAME] = "zookeeper" + str(zk)
+            zookeeper[ZOOKEEPER_ID] = str(zk)
+            zookeeper[ZOOKEEPER_PORT] = "2181"
+            zookeeper[ZOOKEEPER_JMX_PORT] = "9999"
+
+            zookeepers.append(zookeeper)
+
+        self.zookeeper_containers = "{}\n".format(self.broker_offset).\
+            join( [ '- ' + x[ZOOKEEPER_NAME] for x in zookeepers ] )
+        self.zookeeper_ports = ",".join( [ x[ZOOKEEPER_NAME] + x[ZOOKEEPER_PORT] for x in zookeepers ] )
+        self.zookeeper_internal_ports = ";".join( [ x[ZOOKEEPER_NAME] + ":2888:3888" for x in zookeepers ] )
+
+        for zk in zookeepers:
+            zk[ZOOKEEPER_INTERNAL_PORTS] = self.zookeeper_internal_ports
+
+        services = "\n".join( [ self.generate_one_zookeeper_service(x) for x in zookeepers ] )
+
+        return services
+
+    def generate_one_zookeeper_service(self, zookeeper):
+        service = self.zookeeper_template
+        for key,value in zookeeper.items():
+            service = service.replace(key, value)
+
+        lines = service.split('\n')
+        result = [ self.zookeeper_offset + line for line in lines ]
+
+        return "\n".join(result)
 
     def generate_broker_services(self):
-        pass
+        brokers = []
+
+        for id in range(1,self.args.brokers + 1):
+            broker = {}
+            broker[BROKER_NAME] = "kafka" + str(id)
+            broker[BROKER_ID] = str(id)
+            broker[BROKER_PORT_INTERNAL] = "{}:{}".format(broker[BROKER_NAME],"19092")
+            broker[BROKER_PORT_EXTERNAL] = "{}:{}".format(broker[BROKER_NAME],"9092")
+            broker[BROKER_ADVERTISED_PORT_INTERNAL] = "{}:{}".format(broker[BROKER_NAME],"19092")
+            broker[BROKER_ADVERTISED_PORT_EXTERNAL] = "{}:{}".format("localhost","9092")
+            broker[BROKER_JMX_PORT] = "9999"
+            broker[ZOOKEEPER_CONTAINERS] = self.zookeeper_containers
+            broker[ZOOKEEPER_PORTS] = self.zookeeper_ports
+
+            brokers.append(broker)
+
+        services = "\n".join( [ self.generate_one_broker_service(x) for x in brokers ] )
+
+        return services
+
+    def generate_one_broker_service(self, broker):
+        service = self.broker_template
+        for key,value in broker.items():
+            service = service.replace(key,value)
+
+        lines = service.split('\n')
+        result = [ self.broker_offset + line for line in lines ]
+
+        return "\n".join(result)
 
     @staticmethod
     def find_offset(template, placeholder):
+        lines = template.split("\n")
         pattern = re.compile(r"^(\s*){}".format(placeholder))
-        for line in template:
+        for line in lines:
             match = pattern.match(line)
             if match:
                 return match.group(1)
@@ -81,7 +158,7 @@ if __name__ == '__main__':
 
     # required without defaults
     parser.add_argument('-b', '--brokers', required=True, type=int, help="Number of Brokers")
-    parser.add_argument('-z', '--zookeeper', required=True, type=int, help="Number of ZooKeepers")
+    parser.add_argument('-z', '--zookeepers', required=True, type=int, help="Number of ZooKeepers")
 
     # optional with defaults
     parser.add_argument('--docker-compose-template', default=DOCKER_COMPOSE_TEMPLATE,
@@ -93,7 +170,6 @@ if __name__ == '__main__':
     parser.add_argument('--docker-compose-file', default=DOCKER_COMPOSE_FILE,
                         help="Output file for docker-compose, default \"{}\"".format(DOCKER_COMPOSE_FILE))
 
-    parser.add_argument
     args = parser.parse_args()
 
     generator = YamlGenerator(args)
