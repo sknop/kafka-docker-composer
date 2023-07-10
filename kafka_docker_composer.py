b5cebb7775a20e4f6a6fc2af3c5dfc00b602452d
Sven Erik Knop <seknop@gmail.com>
2023-07-10 17:49:46 +0100

    Working version with Controllers

--- kafka_docker_composer.py
+++ kafka_docker_composer.py
@@ -7,6 +7,8 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 
 # constants
 
+RANDOM_UUID = "Nk018hRAQFytWskYqtQduw"
+
 DEFAULT_RELEASE = "7.4.0"
 JMX_PROMETHEUS_JAVA_AGENT_VERSION = "0.18.0"
 JMX_PORT = "8091"
@@ -14,6 +16,9 @@ JMX_JAR_FILE = f"jmx_prometheus_javaagent-{JMX_PROMETHEUS_JAVA_AGENT_VERSION}.ja
 JMX_PROMETHEUS_JAVA_AGENT = f"-javaagent:/tmp/{JMX_JAR_FILE}={JMX_PORT}:/tmp/"
 JMX_EXTERNAL_PORT = 10000
 JMX_AGENT_PORT = 10100
+
+BROKER_EXTERNAL_BASE_PORT = 9090
+BROKER_INTERNAL_BASE_PORT = 19090
 
 LOCAL_VOLUMES = "$PWD/volumes/"
 
@@ -39,9 +44,11 @@ class DockerComposeGenerator:
         )
 
         self.zookeepers = ""
-        self.controllers = ""
+        self.quorum_voters = ""
         self.bootstrap_servers = ""
         self.schema_registries = ""
+
+        self.controllers = []
 
         self.controller_containers = []
         self.zookeeper_containers = []
@@ -50,16 +57,33 @@ class DockerComposeGenerator:
 
         self.prometheus_jobs = []
 
-        self.external_port_counter = JMX_EXTERNAL_PORT
+        self.jmx_external_port_counter = JMX_EXTERNAL_PORT
         self.agent_port_counter = JMX_AGENT_PORT
 
-    def next_external_port(self):
-        self.external_port_counter += 1
-        return self.external_port_counter
+        self.use_kraft = self.args.controllers > 0
+        self.node_id = 0
+        self.internal_port = BROKER_INTERNAL_BASE_PORT
+        self.external_port = BROKER_EXTERNAL_BASE_PORT
+
+    def next_jmx_external_port(self):
+        self.jmx_external_port_counter += 1
+        return self.jmx_external_port_counter
 
     def next_agent_port(self):
         self.agent_port_counter += 1
         return self.agent_port_counter
+
+    def next_node_id(self):
+        self.node_id += 1
+        return self.node_id
+
+    def next_internal_broker_port(self):
+        self.internal_port += 1
+        return self.internal_port
+
+    def next_external_broker_port(self):
+        self.external_port += 1
+        return self.external_port
 
     def generate(self):
         self.generate_services()
@@ -104,15 +128,15 @@ class DockerComposeGenerator:
         rack = 0
 
         controllers = []
-        bootstrap_servers = []
+        quorum_voters = []
 
-        for controller_id in range(1, self.args.controllers + 1):
-            port = 9060 + controller_id
-            internal_port = 19060 + controller_id
+        for counter in range(1, self.args.controllers + 1):
+            port = self.next_internal_broker_port()
+            node_id = self.next_node_id()
 
             controller = {}
 
-            name = self.create_name("controller", controller_id)
+            name = self.create_name("controller", counter)
             controller["name"] = name
             controller["hostname"] = name
             controller["container_name"] = name
@@ -120,19 +144,18 @@ class DockerComposeGenerator:
             controller["image"] = f"confluentinc/{KAFKA_CONTAINER}:" + self.args.release
 
             controller["environment"] = {
-                "KAFKA_NODE_ID": controller_id,
-                "KAFKA_LISTENERS": f"CONTROLLER://{name}:{internal_port}, EXTERNAL://{name}:{port}",
-                "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP": "CONTROLLER:PLAINTEXT,EXTERNAL:PLAINTEXT",
-                "KAFKA_ADVERTISED_LISTENERS": f"CONTROLLER://{name}:{internal_port}, EXTERNAL://localhost:{port}",
-                "KAFKA_INTER_BROKER_LISTENER_NAME": "PLAINTEXT",
+                "KAFKA_NODE_ID": node_id,
+                "CLUSTER_ID": self.args.uuid,
+                "KAFKA_PROCESS_ROLES": "controller",
+                "KAFKA_LISTENERS": f"CONTROLLER://{name}:{port}",
+                "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP": "CONTROLLER:PLAINTEXT",
+                "KAFKA_INTER_BROKER_LISTENER_NAME": "CONTROLLER",
+                "KAFKA_CONTROLLER_LISTENER_NAMES": "CONTROLLER",
+                "KAFKA_CONTROLLER_QUORUM_VOTERS": "# Need to set #",
                 "KAFKA_JMX_PORT": 9999,
                 "KAFKA_JMX_HOSTNAME": name,
                 "KAFKA_BROKER_RACK": f"rack-{rack}",
                 "KAFKA_OPTS": JMX_PROMETHEUS_JAVA_AGENT + BROKER_JMX_CONFIG
-            }
-
-            controller["ports"] = {
-                port: port
             }
 
             controller["volumes"] = [
@@ -141,12 +164,17 @@ class DockerComposeGenerator:
             ]
 
             controllers.append(controller)
-            bootstrap_servers.append(f"{name}:{internal_port}")
+            quorum_voters.append(f"{node_id}@{name}:{port}")
 
             rack = DockerComposeGenerator.next_rack(rack, self.args.racks)
 
-        self.bootstrap_servers = ",".join(bootstrap_servers)
-        self.broker_containers = [b["name"] for b in controllers]
+        self.controller_containers = [b["name"] for b in controllers]
+        self.quorum_voters = ",".join(quorum_voters)
+
+        for controller in controllers:
+            controller["environment"]["KAFKA_CONTROLLER_QUORUM_VOTERS"] = self.quorum_voters
+
+        self.controllers = controllers
 
         return controllers
 
@@ -198,7 +226,7 @@ class DockerComposeGenerator:
 
             zookeeper["image"] = "confluentinc/cp-zookeeper:" + self.args.release
 
-            jmx_port = self.next_external_port()
+            jmx_port = self.next_jmx_external_port()
 
             environment = {
                 "ZOOKEEPER_SERVER_ID": zk,
@@ -256,8 +284,9 @@ class DockerComposeGenerator:
         self.prometheus_jobs.append(job)
 
         for broker_id in range(1, self.args.brokers + 1):
-            port = 9090 + broker_id
-            internal_port = 19090 + broker_id
+            port = self.next_external_broker_port()
+            internal_port = self.next_internal_broker_port()
+            node_id = self.next_node_id()
 
             broker = {}
 
@@ -270,13 +299,11 @@ class DockerComposeGenerator:
 
             broker["image"] = f"confluentinc/{KAFKA_CONTAINER}:" + self.args.release
 
-            broker["depends_on"] = self.zookeeper_containers
+            broker["depends_on"] = self.controller_containers if self.use_kraft else self.zookeeper_containers
 
-            jmx_port = self.next_external_port()
+            jmx_port = self.next_jmx_external_port()
 
             broker["environment"] = {
-                "KAFKA_BROKER_ID": broker_id,
-                "KAFKA_ZOOKEEPER_CONNECT": self.zookeepers,
                 "KAFKA_LISTENERS": f"PLAINTEXT://{name}:{internal_port}, EXTERNAL://{name}:{port}",
                 "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP": "PLAINTEXT:PLAINTEXT,EXTERNAL:PLAINTEXT",
                 "KAFKA_ADVERTISED_LISTENERS": f"PLAINTEXT://{name}:{internal_port}, EXTERNAL://localhost:{port}",
@@ -288,6 +315,22 @@ class DockerComposeGenerator:
                 "KAFKA_OPTS": JMX_PROMETHEUS_JAVA_AGENT + BROKER_JMX_CONFIG,
                 "KAFKA_METRIC_REPORTERS": "io.confluent.metrics.reporter.ConfluentMetricsReporter",
             }
+
+            controller_dict = {}
+
+            if self.use_kraft:
+                controller_dict["KAFKA_NODE_ID"] = node_id
+                controller_dict["CLUSTER_ID"] = self.args.uuid
+                controller_dict["KAFKA_CONTROLLER_QUORUM_VOTERS"] = self.quorum_voters
+                controller_dict["KAFKA_PROCESS_ROLES"] = 'broker'
+                controller_dict["KAFKA_CONTROLLER_LISTENER_NAMES"] = "CONTROLLER"
+                controller_dict["KAFKA_LISTENER_SECURITY_PROTOCOL_MAP"] = \
+                    "CONTROLLER:PLAINTEXT" + "," + broker["environment"]["KAFKA_LISTENER_SECURITY_PROTOCOL_MAP"]
+            else:
+                controller_dict["KAFKA_BROKER_ID"] = broker_id
+                controller_dict["KAFKA_ZOOKEEPER_CONNECT"] = self.zookeepers
+
+            broker["environment"].update(controller_dict)
 
             broker["ports"] = {
                 port: port,
@@ -317,6 +360,10 @@ class DockerComposeGenerator:
 
         for broker in brokers:
             broker["environment"]["KAFKA_CONFLUENT_METRICS_REPORTER_BOOTSTRAP_SERVERS"] = \
+                self.bootstrap_servers
+
+        for controller in self.controllers:
+            controller["environment"]["KAFKA_CONFLUENT_METRICS_REPORTER_BOOTSTRAP_SERVERS"] = \
                 self.bootstrap_servers
 
         return brokers
@@ -492,6 +539,9 @@ if __name__ == '__main__':
     parser.add_argument('--control-center', default=False, action='store_true',
                         help="Include Confluent Control Center [False]")
 
+    parser.add_argument('--uuid', type=str, default=RANDOM_UUID,
+                        help=f"UUID of the cluster [{RANDOM_UUID}]")
+
     parser.add_argument('-p', '--prometheus', default=False, action='store_true', help="Include Prometheus [False]")
 
     parser.add_argument('--kafka-container', default=KAFKA_CONTAINER,
@@ -513,7 +563,7 @@ if __name__ == '__main__':
         args = load_configfile(args, args.config)
 
     # Check for inconsistencies
-    if (args.zookeepers or args.zookeeper_groups) and args.controllers:
+    if args.zookeepers and args.controllers:
         print("Zookeeper and Kafka Controllers (KRaft) are mutually exclusive", file=sys.stderr)
         sys.exit(2)
 
