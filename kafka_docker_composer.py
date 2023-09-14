619036e61c4364637fb9c721ca6c75aaebf95ba1
Sven Erik Knop <seknop@gmail.com>
2023-09-14 13:01:34 +0100

    Add ksqlDB to services that can be started

--- kafka_docker_composer.py
+++ kafka_docker_composer.py
@@ -50,6 +50,7 @@ class DockerComposeGenerator:
         self.schema_registries = ""
         self.schema_registry_urls = ""
         self.connect_urls = ""
+        self.ksqldb_urls = ""
 
         self.controllers = []
 
@@ -58,6 +59,7 @@ class DockerComposeGenerator:
         self.broker_containers = []
         self.connect_containers = []
         self.schema_registry_containers = []
+        self.ksqldb_containers = []
 
         self.prometheus_jobs = []
 
@@ -106,6 +108,7 @@ class DockerComposeGenerator:
         services += self.generate_broker_services()
         services += self.generate_schema_registry_service()
         services += self.generate_connect_services()
+        services += self.generate_ksqldb_services()
         services += self.generate_control_center_service()
         services += self.generate_prometheus_service()
         services += self.generate_grafana_service()
@@ -529,7 +532,64 @@ class DockerComposeGenerator:
 
         self.connect_urls = ",".join(connect_hosts)
 
+        if self.args.connect_instances > 0:
+            self.prometheus_jobs.append(job)
+
         return connects
+
+    def generate_ksqldb_services(self):
+        ksqldbs = []
+        targets = []
+        ksqldb_hosts = []
+
+        job = {
+            "name": "ksqldb",
+            "scrape_interval": "5s",
+            "targets": targets
+        }
+
+        for ksqldb_id in range(1, self.args.ksqldb_instances + 1):
+            port = 8087 + ksqldb_id
+
+            name = self.create_name("ksqldb", ksqldb_id)
+
+            ksqldb = {
+                'name': name,
+                "hostname": name,
+                "container_name": name,
+                "image": "confluentinc/cp-ksqldb-server:" + self.args.release,
+                "depends_on_condition": self.broker_containers + self.schema_registry_containers,
+                "environment": {
+                    "KSQL_LISTENERS": f"http://0.0.0.0:{port}",
+                    "KSQL_BOOTSTRAP_SERVERS": self.bootstrap_servers,
+                    "KSQL_KSQL_LOGGING_PROCESSING_STREAM_AUTO_CREATE": "true",
+                    "KSQL_KSQL_LOGGING_PROCESSING_TOPIC_AUTO_CREATE": "true",
+                    "KSQL_KSQL_CONNECT_URL": self.connect_urls,
+                    "KSQL_KSQL_SCHEMA_REGISTRY_URL": self.schema_registry_urls,
+                    "KSQL_KSQL_SERVICE_ID": "kafka-docker-composer",
+                    "KSQL_KSQL_HIDDEN_TOPICS": "^_.*"
+                },
+                "ports": {
+                    port: port
+                },
+                "healthcheck": {
+                    "test": f"curl -fail --silent http://{name}:{port}/healthcheck --output /dev/null || exit 1",
+                    "interval": "10s",
+                    "retries": "20",
+                    "start_period": "20s"
+                },
+                "volumes": [
+                    LOCAL_VOLUMES + JMX_JAR_FILE + ":/tmp/" + JMX_JAR_FILE
+                ]
+            }
+
+            ksqldbs.append(ksqldb)
+            ksqldb_hosts.append(f"http://{name}:{port}")
+            self.ksqldb_containers.append(name)
+
+        self.ksqldb_urls = ",".join(ksqldb_hosts)
+
+        return ksqldbs
 
     def generate_control_center_service(self):
         control_centers = []
@@ -541,12 +601,13 @@ class DockerComposeGenerator:
                 "container_name": "control-center",
                 "image": "confluentinc/cp-enterprise-control-center:" + self.args.release,
                 "depends_on_condition":
-                    self.broker_containers + self.schema_registry_containers + self.connect_containers,
+                    self.broker_containers + self.schema_registry_containers + self.connect_containers + self.ksqldb_containers,
                 "environment": {
                     "CONTROL_CENTER_BOOTSTRAP_SERVERS": self.bootstrap_servers,
                     "CONTROL_CENTER_SCHEMA_REGISTRY_URL": self.schema_registry_urls,
                     "CONTROL_CENTER_REPLICATION_FACTOR": self.replication_factor(),
-                    "CONTROL_CENTER_CONNECT_CONNECT_CLUSTER": self.connect_urls
+                    "CONTROL_CENTER_CONNECT_CONNECT_CLUSTER": self.connect_urls,
+                    "CONTROL_CENTER_KSQL_KSQL_URL": self.ksqldb_urls
                 },
                 "ports": {
                     9021: 9021
@@ -645,6 +706,8 @@ if __name__ == '__main__':
                         help="Number of Schema Registry instances [0]")
     parser.add_argument('-C', '--connect-instances', default=0, type=int,
                         help="Number of Kafka Connect instances [0]")
+    parser.add_argument('-k', '--ksqldb-instances', default=0, type=int,
+                        help="Numer of ksqlDB instances [0]")
     parser.add_argument('--control-center', default=False, action='store_true',
                         help="Include Confluent Control Center [False]")
 
